edc24eb8ae296f1d4ce125d93af26781800945f7
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-06-16 14:05:20 -0700

    Initial commit

---
+++ src/main/java/com/revature/ExpenseReimbursementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReimbursementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReimbursementSystemApplication.class, args);
+	}
+
+}
