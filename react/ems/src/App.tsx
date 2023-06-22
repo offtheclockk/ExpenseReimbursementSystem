f1de56b7b066c5128b8c6dc8ad56be596c1418e5
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-06-22 16:25:46 -0700

    App Changes

--- react/ems/src/App.tsx
+++ react/ems/src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
-import "./App.css";
-import UserPage from "./components/User";
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+import UsersComponent from "./components/UsersComponent";
+import ReimbursementsComponent from "./components/ReimbursementsComponent";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
   return (
-    <div className="App">
-      <LoginPage />
+    <div>
+      <h1>Expense Reimbursement System</h1>
+      {isLoggedIn && <Header />}
+      <Switch>
+        <Route exact path="/auth/login">
+          <LoginPage onLogin={handleLogin} />
+        </Route>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/users" component={UsersComponent} />
+        <Route path="/reimbursements" component={ReimbursementsComponent} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
