886ddac5f93e2ffeccc032db054cdd5b80e49e8b
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-06-29 07:33:55 -0700

    dashboard changes

---
+++ react/ems/src/components/Dashboards/EmployeeDashboard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const EmployeeDashboard: React.FC = () => {
+  return (
+    <div className="container">
+      <div className="jumbotron mt-4">
+        <h1 className="display-4">Dashboard</h1>
+        <p className="lead">Welcome to the Employee dashboard!</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeDashboard;
